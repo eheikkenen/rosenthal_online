c9100ab4441eae547c240b55e076b18f649f2370
Eric Heikkenen <eheikken@vols.utk.edu>
2020-03-21 10:56:09 -0400

    added rosenthal function

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("hello world")
+
+const rosenthal = (x, y, z, initialTemperature, power, lambda, velocity, alpha) => {
+  const r = Math.sqrt(x*x + y*y + z*z)
+  const dt = power / (2 * Math.PI * lambda * r) * Math.exp(-velocity / (2 * alpha) * (r + x))
+  return initialTemperature + dt
+}
+
