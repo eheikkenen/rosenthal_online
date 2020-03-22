dad3be1a3b955e95f83ce4836824e9573d62f609
Eric Heikkenen <eheikken@vols.utk.edu>
2020-03-22 13:26:41 -0400

    removed highlighting for now

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ const setDefaults316 = () => {
     elements["rho"].value = 8000;
     elements["cp"].value = 500;
     elements["meltingTemperature"].value = 1660;
-    setButtonActive();
+    // setButtonActive();
     drawPlots();
 };
 
@@ -179,7 +179,7 @@ const setDefaultsTitanium = () => {
     elements["rho"].value = 4430;
     elements["cp"].value = 526;
     elements["meltingTemperature"].value = 1900;
-    setButtonActive();
+    // setButtonActive();
     drawPlots();
 };
 
@@ -190,7 +190,7 @@ const setDefaultsInconel = () => {
     elements["rho"].value = 8200;
     elements["cp"].value = 435;
     elements["meltingTemperature"].value = 1575;
-    setButtonActive();
+    // setButtonActive();
     drawPlots();
 };
 
@@ -201,23 +201,8 @@ const setDefaultsAluminum = () => {
     elements["rho"].value = 2670;
     elements["cp"].value = 900;
     elements["meltingTemperature"].value = 850;
-    setButtonActive();
+    // setButtonActive();
     drawPlots();
 };
 
 onFormChange();
-
-// // Get the container element
-// const btnContainer = document.getElementById("pill-nav");
-//
-// // Get all buttons with class="btn" inside the container
-// const btns = btnContainer.getElementsByClassName("btn");
-//
-// // Loop through the buttons and add the active class to the current/clicked button
-// for (let i = 0; i < btns.length; i++) {
-//     btns[i].addEventListener("click", function() {
-//         const current = document.getElementsByClassName("active");
-//         current[0].className = current[0].className.replace(" active", "");
-//         this.className += " active";
-//     });
-// }
