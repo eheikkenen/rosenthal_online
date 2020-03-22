3bf99a37f9c46b030e0d1ca1cdef844856d907f9
Eric Heikkenen <eheikken@vols.utk.edu>
2020-03-21 21:13:56 -0400

    modified default values formatting fixes

--- app.js
+++ app.js
@@ -1,147 +1,144 @@
 const modelParams = {
-  x: { min: 0.001, max: 0.010, step: 0.001, default: 0.003 },
-  y: { min: 0.001, max: 0.010, step: 0.001, default: 0.001 },
-  z: { min: 0.001, max: 0.010, step: 0.001, default: 0.001 },
-  initialTemp: { min: 273, max: 2000, step: 25, default: 100 },
-  power: { min: 1, max: 100, step: 10, default: 50 },
-  thermalConductivity: { min: 1, max: 500, step: 1, default: 20 },
-  velocity: { min: 0.001, max: 5, step: 0.01, default: 1 }
+    x: {min: 0.001, max: 0.010, step: 0.001, default: 0.003},
+    y: {min: 0.001, max: 0.010, step: 0.001, default: 0.001},
+    z: {min: 0.001, max: 0.010, step: 0.001, default: 0.001},
+    initialTemp: {min: 273, max: 2000, step: 25, default: 393},
+    power: {min: 1, max: 5000, step: 10, default: 200},
+    thermalConductivity: {min: 1, max: 500, step: 1, default: 20},
+    velocity: {min: 0.001, max: 5, step: 0.01, default: 1}
 };
 
 const modelParamDefaults = Object.fromEntries(
-  Object.
-  entries(modelParams).
-  map(
-    ([paramName, paramValues]) => [paramName, paramValues.default]
-  )
+    Object.entries(modelParams).map(
+        ([paramName, paramValues]) => [paramName, paramValues.default]
+    )
 );
 
 const setFormFromUrlBar = () => {
-  const params = getUrlParams();
-  const elements = formElements();
+    const params = getUrlParams();
+    const elements = formElements();
 
-  for (key in params) {
-    elements[key].value = params[key];
-    elements[key].min = modelParams[key].min
-    elements[key].max = modelParams[key].max
-    elements[key].step = modelParams[key].step
-  }
+    for (key in params) {
+        elements[key].value = params[key];
+        elements[key].min = modelParams[key].min
+        elements[key].max = modelParams[key].max
+        elements[key].step = modelParams[key].step
+    }
 
 }
 
 const setUrlBarFromForm = () => {
-  const searchString = (new URLSearchParams(formValues())).toString();
-  history.replaceState(null, '', "?" + searchString)
+    const searchString = (new URLSearchParams(formValues())).toString();
+    history.replaceState(null, '', "?" + searchString)
 }
 
 var foo;
 
 const getUrlParams = () => {
-  let params = {...modelParamDefaults};
-  const searchParams = new URLSearchParams(window.location.search);
+    let params = {...modelParamDefaults};
+    const searchParams = new URLSearchParams(window.location.search);
 
-  for (const [key, value] of searchParams) {
-    if (key in modelParams) {
-      foo = searchParams;
-      const {min: min, max: max} = modelParams[key];
-      if (value != "" && Number(value) >= min && Number(value) <= max) {
-        params[key] = Number(value); 
-      };
+    for (const [key, value] of searchParams) {
+        if (key in modelParams) {
+            foo = searchParams;
+            const {min: min, max: max} = modelParams[key];
+            if (value !== "" && Number(value) >= min && Number(value) <= max) {
+                params[key] = Number(value);
+            }
+        }
     }
-  }
-  return params
+    return params
 };
 
 const onFormChange = () => {
-  setUrlBarFromForm();
-  setFormFromUrlBar();
-  drawPlots();
-}
+    setUrlBarFromForm();
+    setFormFromUrlBar();
+    drawPlots();
+};
 
 const formElements = () => ({
-  x: document.getElementById("x"),
-  y: document.getElementById("y"),
-  z: document.getElementById("z"),
-  power: document.getElementById("power"),
-  initialTemp: document.getElementById("initialTemp"),
-  velocity: document.getElementById("velocity"),
-  thermalConductivity: document.getElementById("thermalConductivity"),
-  xyGraph: document.getElementById("xyGraph"),
-  xzGraph: document.getElementById("xzGraph"),
-})
+    x: document.getElementById("x"),
+    y: document.getElementById("y"),
+    z: document.getElementById("z"),
+    power: document.getElementById("power"),
+    initialTemp: document.getElementById("initialTemp"),
+    velocity: document.getElementById("velocity"),
+    thermalConductivity: document.getElementById("thermalConductivity"),
+    xyGraph: document.getElementById("xyGraph"),
+    xzGraph: document.getElementById("xzGraph"),
+});
 
 const formValues = () => {
-  const elements = formElements();
-  return Object.fromEntries(
-    Object.
-    keys(modelParams).
-    map(param => [param, Number(elements[param].value)])
+    const elements = formElements();
+    return Object.fromEntries(
+        Object.keys(modelParams).map(param => [param, Number(elements[param].value)])
     )
-}
+};
 
 const rosenthal = (x, y, z, initialTemperature, power, lambda, velocity, alpha) => {
-  const r = Math.sqrt(x*x + y*y + z*z);
-  const dt = power / (2 * Math.PI * lambda * r) * Math.exp(-velocity / (2 * alpha) * (r + x));
-  return initialTemperature + dt;
+    const r = Math.sqrt(x * x + y * y + z * z);
+    const dt = power / (2 * Math.PI * lambda * r) * Math.exp(-velocity / (2 * alpha) * (r + x));
+    return initialTemperature + dt;
 };
 
 const drawPlots = () => {
 
-  const values = formValues();
+    const values = formValues();
 
-  const steps = 500;
+    const steps = 500;
 
-  // declare vars
-  var size = 500, x = new Array(size), y = new Array(size), z = new Array(size), i, j;
+    // declare vars
+    const size = 500, x = new Array(size), y = new Array(size), z = new Array(size);
 
-  // get x and y values
-  for (var i = 0; i < size; i++) {
-  x[i] = -values.x + (2 * values.x / size) * i;
-  y[i] = -values.y + (2 * values.y / size) * i;
-  z[i] = new Array(size)
-  }
-
-  // get thermal diffusivity
-  const alpha = values.thermalConductivity / (8000 * 460);
-  
-  // get temperatures
-  for (let i = 0; i < steps; i++) {
-    for (let j = 0; j < steps; j++) {
-      //debugger;
-      z[j][i] = rosenthal(x[i], y[j], 0, values.initialTemp, values.power, values.thermalConductivity, values.velocity, alpha);
-      if (z[j][i] > 3000) {z[j][i] = 3000;}
-      }
+    // get x and y values
+    for (let i = 0; i < size; i++) {
+        x[i] = -values.x + (2 * values.x / size) * i;
+        y[i] = -values.y + (2 * values.y / size) * i;
+        z[i] = new Array(size)
     }
 
-    var data = [ {
-      z: z,
-      x: x,
-      y: y,
-      type: 'contour',
-      contours: {
-        coloring: 'heatmap'
-	}
-      }
+    // get thermal diffusivity
+    const alpha = values.thermalConductivity / (8000 * 460);
+
+    // get temperatures
+    for (let i = 0; i < steps; i++) {
+        for (let j = 0; j < steps; j++) {
+            z[j][i] = rosenthal(x[i], y[j], 0, values.initialTemp, values.power, values.thermalConductivity, values.velocity, alpha);
+            if (z[j][i] > 3000) {
+                z[j][i] = 3000;
+            }  // set maximum value
+        }
+    }
+
+    const data = [{
+        z: z,
+        x: x,
+        y: y,
+        type: 'contour',
+        contours: {
+            coloring: 'heatmap'
+        }
+    }
     ];
 
     //var layout = {
     //  title: 'X-Y Plot',
     //}
 
-    var layout = {
-      title: {
-        text: 'X-Y Plot'
-      },
-      xaxis: {
-        text: 'X Axis [m]'
-      },
-      YAXIS: {
-        text: 'Y Axis [m]'
-      }
-    }
+    const layout = {
+        title: {
+            text: 'X-Y Plot'
+        },
+        xaxis: {
+            text: 'X Axis [m]'
+        },
+        YAXIS: {
+            text: 'Y Axis [m]'
+        }
+    };
 
     Plotly.newPlot('plotlyDiv', data, layout);
-}
+};
 
 onFormChange();
 
