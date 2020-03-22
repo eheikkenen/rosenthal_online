458133be67cb3463efa9ae3fd14d355da7d1b3b0
Eric Heikkenen <eheikken@vols.utk.edu>
2020-03-22 01:23:53 -0400

    highlighting is maybe starting to work

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ const setButtonActive = () => {
     const btnContainer = document.getElementById("selectableMaterials");
 
     // Get all buttons with class="btn" inside the container
-    const btns = btnContainer.getElementsByClassName("pill-nav");
+    const btns = btnContainer.getElementsByClassName("btn");
 
     // Loop through the buttons and add the active class to the current/clicked button
     for (let i = 0; i < btns.length; i++) {
@@ -206,3 +206,18 @@ const setDefaultsAluminum = () => {
 };
 
 onFormChange();
+
+// // Get the container element
+// const btnContainer = document.getElementById("pill-nav");
+//
+// // Get all buttons with class="btn" inside the container
+// const btns = btnContainer.getElementsByClassName("btn");
+//
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (let i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function() {
+//         const current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
